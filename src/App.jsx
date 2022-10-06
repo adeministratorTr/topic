1fe7745777ed3954b076df9d2fb71550e8a332c9
adeministratorTr <adembavas@gmail.com>
2022-10-06 18:08:54 +0200

    Move header to component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from 'pages/Home';
+import Header from 'components/Header';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
