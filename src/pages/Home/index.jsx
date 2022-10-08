a5bd65e5ee78b0631f3541d0d65df3bca890ca2c
adeministratorTr <adembavas@gmail.com>
2022-10-08 10:38:15 +0200

    Update Home page with Topic & TopicDetail usage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,32 +1,39 @@
 import { useEffect, useState } from 'react';
 
+import Topic from 'components/Topic';
 import TopicDetail from './components/TopicDetail';
 import { getAllTopics } from 'services/topic';
-import { mapTopics } from './mapper';
+import { mapTopics } from './mapper/mapper';
 
 import './style.css';
 
 export default function Home() {
   const [topics, setTopics] = useState({});
   const [selectedTopic, setSelectedTopic] = useState({});
+
   useEffect(() => {
     const result = getAllTopics();
     setTopics(mapTopics(result.topics));
   }, []);
+
   return (
     <div>
       <h1>Home page</h1>
-      <div className="container">
+      <div className="home-page-container">
         <div>
           {Array.isArray(topics) &&
             topics.length > 0 &&
             topics.map((topic) => (
-              <p key={topic.id} onClick={() => setSelectedTopic(topic)}>
+              <Topic
+                key={topic.id}
+                score={topic.sentiment.range}
+                onClick={() => setSelectedTopic(topic)}
+              >
                 {topic.label}
-              </p>
+              </Topic>
             ))}
         </div>
-        <div>
+        <div className="topic-detail-container">
           {selectedTopic && selectedTopic.id && (
             <TopicDetail
               text={selectedTopic.label}
