66f2ba0ae77969e797fa42e80a012ec568e6bb07
adeministratorTr <adembavas@gmail.com>
2022-10-08 19:31:17 +0200

    Update app test

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -5,5 +5,8 @@ describe('App', () => {
   it('should render by default', () => {
     render(<App />);
     expect(screen.getByTestId('App')).toBeVisible();
+
+    // There must be an H1 title in the app
+    expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
   });
 });
