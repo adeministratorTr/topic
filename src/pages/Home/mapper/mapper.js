d2528f4eb1bfbf52539bcc5ed771593f6c1ac592
adeministratorTr <adembavas@gmail.com>
2022-10-08 18:46:15 +0200

    Add popularity range to mapper

--- src/pages/Home/mapper/mapper.js
+++ src/pages/Home/mapper/mapper.js
@@ -1,21 +1,25 @@
+// if you are changing this number, Dont forget to update Topic component style/classname
 const DIFFERENT_TEXT_SIZE_NUMBER = 6;
 
 export function mapTopics(topics) {
-  const popularityRange = {
-    min: Math.min(...topics.map((t) => t.volume)),
-    max: Math.max(...topics.map((t) => t.volume))
-  };
+  const min = Math.min(...topics.map((t) => t.volume));
+  const max = Math.max(...topics.map((t) => t.volume));
+  const difference = Math.round((max - min) / DIFFERENT_TEXT_SIZE_NUMBER);
 
   return topics.map(({ id, label, sentiment, sentimentScore, volume }) => {
     return {
       id,
-      label,
+      value: label,
       sentiment: {
         ...sentiment,
         score: sentimentScore,
         range: getScoreColor({ score: sentimentScore })
       },
-      volume
+      count: volume,
+      popularity: getPopularityCategory({
+        volume,
+        difference
+      })
     };
   });
 }
@@ -24,4 +28,8 @@ function getScoreColor({ score }) {
   if (score > 60) return 'high';
   if (score > 40 && score < 60) return 'normal';
   return 'low';
+}
+
+function getPopularityCategory({ volume, difference }) {
+  return Math.round(volume / difference);
 }
--- src/pages/Home/mapper/mapper.spec.js
+++ src/pages/Home/mapper/mapper.spec.js
@@ -4,12 +4,12 @@ const sampleInput = [
   {
     id: '1751295897__Berlin',
     label: 'Berlin',
-    volume: 165,
+    volume: 161,
     type: 'topic',
     sentiment: {
       negative: 3,
-      neutral: 133,
-      positive: 29
+      neutral: 130,
+      positive: 28
     },
     sentimentScore: 65,
     burst: 13,
@@ -125,7 +125,7 @@ const sampleInput = [
   {
     id: '1751295897__Hammered',
     label: 'Hammered',
-    volume: 48,
+    volume: 35,
     type: 'topic',
     sentiment: {
       neutral: 18,
@@ -181,6 +181,65 @@ const sampleInput = [
         volume: 18
       }
     ]
+  },
+  {
+    id: '1751295897__club culture',
+    label: 'club culture',
+    volume: 13,
+    type: 'topic',
+    sentiment: {
+      neutral: 3
+    },
+    sentimentScore: 50,
+    burst: 0,
+    days: [
+      {
+        date: '2014-06-06T00:00:00.000+0000',
+        volume: 1
+      },
+      {
+        date: '2014-06-04T00:00:00.000+0000',
+        volume: 1
+      },
+      {
+        date: '2014-06-09T00:00:00.000+0000',
+        volume: 0
+      },
+      {
+        date: '2014-06-07T00:00:00.000+0000',
+        volume: 0
+      },
+      {
+        date: '2014-06-08T00:00:00.000+0000',
+        volume: 0
+      },
+      {
+        date: '2014-06-03T00:00:00.000+0000',
+        volume: 0
+      },
+      {
+        date: '2014-06-05T00:00:00.000+0000',
+        volume: 1
+      }
+    ],
+    pageType: {
+      blog: 0,
+      facebook: 0,
+      forum: 1,
+      general: 0,
+      image: 0,
+      news: 2,
+      review: 0,
+      twitter: 0,
+      video: 0
+    },
+    queries: [
+      {
+        id: 1751295897,
+        name: 'Berghain',
+        volume: 3
+      }
+    ]
   }
 ];
 
@@ -189,20 +248,22 @@ describe('mapper/mapTopic', () => {
     const expected = [
       {
         id: '1751295897__Berlin',
-        label: 'Berlin',
-        volume: 165,
+        value: 'Berlin',
+        count: 161,
+        popularity: 6,
         sentiment: {
           negative: 3,
-          neutral: 133,
-          positive: 29,
+          neutral: 130,
+          positive: 28,
           score: 65,
           range: 'high'
         }
       },
       {
         id: '1751295897__DJ',
-        label: 'DJ',
-        volume: 48,
+        value: 'DJ',
+        count: 48,
+        popularity: 2,
         sentiment: {
           neutral: 46,
           positive: 2,
@@ -212,18 +273,33 @@ describe('mapper/mapTopic', () => {
       },
       {
         id: '1751295897__Hammered',
-        label: 'Hammered',
-        volume: 48,
-        type: 'topic',
+        value: 'Hammered',
+        count: 35,
+        popularity: 1,
         sentiment: {
           neutral: 18,
           negative: 30,
           score: 20,
           range: 'low'
         }
+      },
+      {
+        id: '1751295897__club culture',
+        value: 'club culture',
+        count: 13,
+        popularity: 1,
+        sentiment: {
+          neutral: 3,
+          score: 50,
+          range: 'normal'
+        }
       }
     ];
     const result = mapTopics(sampleInput);
     expect(result).toEqual(expected);
+    result.map((topic) => {
+      expect(topic.popularity).toBeGreaterThanOrEqual(1);
+      expect(topic.popularity).toBeLessThanOrEqual(6);
+    });
   });
 });
